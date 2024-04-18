8bd04f71465dd099bc428a2299fa2cdf1890e8d7
苹果喵喵 <149125378+PingGuoMiaoMiao@users.noreply.github.com>
2024-04-18 09:46:27 +0000

    modified:   exercises/hashmaps/hashmaps3.rs

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -39,9 +39,23 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the numyiber of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-        let scores = 
-        
+         // Update team 1's scores
+         let team_1 = scores.entry(team_1_name.clone()).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_1.goals_scored += team_1_score;
+        team_1.goals_conceded += team_2_score;
+
+        // Update team 2's scores
+        let team_2 = scores.entry(team_2_name.clone()).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;
     }
+        
     scores
 }
 
