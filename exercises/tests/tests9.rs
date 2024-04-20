3c568b07f79efb440c48db3db116b0f881a00477
苹果喵喵 <149125378+PingGuoMiaoMiao@users.noreply.github.com>
2024-04-20 12:51:12 +0000

    modified:   exercises/tests/build.rs 	modified:   exercises/tests/tests6.rs 	modified:   exercises/tests/tests7.rs 	modified:   exercises/tests/tests8.rs 	modified:   exercises/tests/tests9.rs

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -30,12 +30,14 @@
 // I AM NOT DONE
 
 extern "Rust" {
+    #[no_mangle]
     fn my_demo_function(a: u32) -> u32;
+    #[no_mangle]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
-    // No `extern` equals `extern "Rust"`.
+    #[no_mangle]
     fn my_demo_function(a: u32) -> u32 {
         a
     }
@@ -47,12 +49,6 @@ mod tests {
 
     #[test]
     fn test_success() {
-        // The externally imported functions are UNSAFE by default
-        // because of untrusted source of other languages. You may
-        // wrap them in safe Rust APIs to ease the burden of callers.
-        //
-        // SAFETY: We know those functions are aliases of a safe
-        // Rust function.
         unsafe {
             my_demo_function(123);
             my_demo_function_alias(456);
